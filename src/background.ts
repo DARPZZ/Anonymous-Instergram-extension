eebc5d28d2695e9c63d5698aac0d2c524691ef12
Darpzz <rasmustofthermansen@gmail.com>
2025-02-26 17:09:54 +0100

    Moved scripts affecting DOM away from background and into content script

--- src/background.ts
+++ src/background.ts
@@ -1,43 +1,11 @@
-const instergramBaseUrl = "https://www.instagram.com/"
-function removeDuplicates()
-{
-    let buttonex = document.getElementsByClassName("seePostButtons");
-    if (buttonex.length > 0) {
-        Array.from(buttonex).forEach((btn) => {
-            btn.parentElement?.removeChild(btn);
-        });
-    }
-}
-function getAllPictures() {
-    const elements = document.getElementsByClassName("x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz _a6hd");
-    removeDuplicates();
-    for (let index = 0; index < elements.length; index++) {
-        const element = elements[index].getAttribute('href');
-        let button = document.createElement('button'); 
-        button.innerText = 'See post';
-        button.style.marginTop = '10px';
-        button.style.zIndex = "200";
-        button.style.width = "100%"
-        button.style.position ="relative";
-        button.style.marginBottom = "5px";
-        button.value = element;
-        button.className = "seePostButtons";
-        if(element.includes("/p/")|| element.includes("/reel/"))
-        {
-            button.addEventListener('click', function() {
-                window.open(`${instergramBaseUrl}${element}`, '_blank');
-            });
-            elements[index].parentElement.appendChild(button);
-        }
-    }
-}
-function runScript(): void {
-    getAllPictures()
-}
-
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id ? tab.id : -1},
-        func: runScript
-    }).then();
+chrome.action.onClicked.addListener(() => {
+   chrome.tabs.query({
+    active : true,
+    currentWindow : true
+   },
+   function (tabs){
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id,{action:"run"})
+   }
+)
 })
