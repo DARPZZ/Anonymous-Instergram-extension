2c2c9dad4b8353ae0246c17496216be56db527e4
Darpzz <rasmustofthermansen@gmail.com>
2025-05-18 23:25:14 +0200

    Added support for firefox and chrome

---
+++ src/dist/firefox/background.js
@@ -0,0 +1,1 @@
+chrome.action.onClicked.addListener((()=>{chrome.tabs.query({active:!0,currentWindow:!0},(function(e){var n=e[0];chrome.tabs.sendMessage(n.id,{action:"run"})}))}));
--- src/webpack/webpack.config.js
+++ src/webpack/webpack.config.js
@@ -1,25 +1,46 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-module.exports = {
-   mode: "production",
-   entry: {
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = (env) => {
+  const browser = env.browser || "chrome";
+  const manifestFile =
+    browser === "firefox" ? "manifest.firefox.json" : "manifest.chrome.json";
+
+  return {
+    mode: "production",
+    entry: {
       background: path.resolve(__dirname, "..", "background.ts"),
       content: path.resolve(__dirname, "..", "content.ts"),
-   },
-   output: {
-      path: path.join(__dirname, "../dist"),
+    },
+    output: {
+      path: path.join(__dirname, `../dist/${browser}`),
       filename: "[name].js",
-   },
-   resolve: {
+    },
+    resolve: {
       extensions: [".ts", ".js"],
-   },
-   module: {
+    },
+    module: {
       rules: [
-         {
-            test: /\.tsx?$/,
-            loader: "ts-loader",
-            exclude: /node_modules/,
-         },
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader",
+          exclude: /node_modules/,
+        },
       ],
-   },
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, "..", manifestFile),
+            to: path.resolve(__dirname, `../dist/${browser}/manifest.json`),
+          },
+          {
+            from: path.resolve(__dirname, "..", "instergram.png"),
+            to: path.resolve(__dirname, `../dist/${browser}/instergram.png`),
+          },
+        ],
+      }),
+    ],
+  };
 };
