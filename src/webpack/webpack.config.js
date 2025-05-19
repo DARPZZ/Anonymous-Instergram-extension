fc07e43cab0d82da74d3d77303833bbbea863af2
Darpzz <rasmustofthermansen@gmail.com>
2025-05-19 02:49:34 +0200

    Revert "Added remove pop up that asked you to login"
    This reverts commit 9d919aabca17711f82188a57e8e1f4c36db17114.

--- src/webpack/webpack.config.js
+++ src/webpack/webpack.config.js
@@ -1,46 +1,25 @@
-const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-
-module.exports = (env) => {
-  const browser = env.browser || "chrome";
-  const manifestFile =
-    browser === "firefox" ? "manifest.firefox.json" : "manifest.chrome.json";
-
-  return {
-    mode: "production",
-    entry: {
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+module.exports = {
+   mode: "production",
+   entry: {
       background: path.resolve(__dirname, "..", "background.ts"),
       content: path.resolve(__dirname, "..", "content.ts"),
-    },
-    output: {
-      path: path.join(__dirname, `../dist/${browser}`),
+   },
+   output: {
+      path: path.join(__dirname, "../dist"),
       filename: "[name].js",
-    },
-    resolve: {
+   },
+   resolve: {
       extensions: [".ts", ".js"],
-    },
-    module: {
+   },
+   module: {
       rules: [
-        {
-          test: /\.tsx?$/,
-          loader: "ts-loader",
-          exclude: /node_modules/,
-        },
+         {
+            test: /\.tsx?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/,
+         },
       ],
-    },
-    plugins: [
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: path.resolve(__dirname, "..", manifestFile),
-            to: path.resolve(__dirname, `../dist/${browser}/manifest.json`),
-          },
-          {
-            from: path.resolve(__dirname, "..", "instergram.png"),
-            to: path.resolve(__dirname, `../dist/${browser}/instergram.png`),
-          },
-        ],
-      }),
-    ],
-  };
+   },
 };
