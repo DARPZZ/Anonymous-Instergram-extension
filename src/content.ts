afcb67fbf972e0bb589f2f9d185201aa9cc6a4d6
Darpzz <rasmustofthermansen@gmail.com>
2025-06-08 05:38:56 +0200

    document.querySelectorAll insted of getElementsByClassName

--- src/content.ts
+++ src/content.ts
@@ -1,4 +1,4 @@
-const instergramBaseUrl = "https://www.instagram.com/"
+const instergramBaseUrl = "https://www.instagram.com"
 
 chrome.runtime.onMessage.addListener( (request, sender, message) => {
     if (request.action === "run") {
@@ -19,7 +19,7 @@ function RemovePopUpsAutomatically() {
         subtree: true
     });
 }
-// calling to active the observer
+
 RemovePopUpsAutomatically();
 
 
@@ -30,10 +30,11 @@ function removeDuplicates()
         Array.from(buttonex).forEach((btn) => {
             btn.parentElement?.removeChild(btn);
         });
+        console.log("Removeing Pop up")
     }
 }
 function getAllPictures() {
-    const elements = document.getElementsByClassName("x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz _a6hd");
+    const elements = document.querySelectorAll('[role="link"]');
     removeDuplicates();
     for (let index = 0; index < elements.length; index++) {
         const element = elements[index].getAttribute('href');
@@ -54,6 +55,7 @@ function getAllPictures() {
             elements[index].parentElement.appendChild(button);
         }
     }
+    console.log("Added buttons");
 }
 function runScript(): void {
     getAllPictures()
